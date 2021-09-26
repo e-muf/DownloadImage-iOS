b4d2b4c8f9c5cb075c79a79b8a319c1b005537f9
Emanuel Flores <emnl.fmtz@gmail.com>
2021-09-26 01:38:53 -0500

    Initial Commit

---
+++ DownloadImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DownloadImage
+//
+//  Created by Emanuel Flores Martínez on 26/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
